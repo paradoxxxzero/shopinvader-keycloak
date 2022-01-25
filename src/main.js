d2a416342d79fa7f802bfec9dc3bc2144e5bb76b
Florian Mounier <paradoxxx.zero@gmail.com>
2022-01-25 11:44:59 +0100

    Use silentCheckSsoRedirectUri to prevent full page redirects

--- src/main.js
+++ src/main.js
@@ -3,12 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import config from '../config.json'
 
-const GUEST_MARK = '___guest___'
-
-const urlHash = ({ add = '', remove = '' } = {}) =>
-  window.location.pathname +
-  window.location.search +
-  ((window.location.hash || '#') + add).replace(remove, '').replace(/^#$/, '')
+const silentCheckSsoRedirectUri = `${window.location.origin}/${config.sso_page}`
 
 ;(async () => {
   const keycloak = new Keycloak(config.keycloak)
@@ -17,37 +12,16 @@ const urlHash = ({ add = '', remove = '' } = {}) =>
   window.keycloak = keycloak
   window.keycloakGuest = keycloakGuest
 
-  // If we are in guest callback, we mustn't try to init normal keycloak as it
-  // will erase the callback state.
-  if (window.location.hash.includes(GUEST_MARK)) {
-    localStorage.setItem('hash', window.location.hash)
-    window.location.hash = ''
-  }
-
   const auth = await keycloak.init({
     onLoad: 'check-sso',
-    // checkLoginIframe: false,
-    // silentCheckSsoRedirectUri: window.location.origin + '/sso.html',
+    silentCheckSsoRedirectUri,
   })
 
-  if (localStorage.getItem('hash')) {
-    window.location.hash = localStorage.getItem('hash')
-    localStorage.removeItem('hash')
-  }
-
-  // if (!auth) {
-  if (!window.location.hash.includes(GUEST_MARK)) {
-    history.replaceState(
-      window.history.state,
-      null,
-      urlHash({ add: GUEST_MARK })
-    )
-  }
   const guestAuth = await keycloakGuest.init({
     onLoad: 'check-sso',
-    // checkLoginIframe: false,
-    // silentCheckSsoRedirectUri: window.location.origin + '/sso.html',
+    silentCheckSsoRedirectUri,
   })
+
   if (!auth && !guestAuth) {
     // No guest auth available, create user:
     const authUrl = `${config.keycloak_guest.url}/realms/${
@@ -55,13 +29,6 @@ const urlHash = ({ add = '', remove = '' } = {}) =>
     }/authorize-guest?redirect_uri=${encodeURIComponent(window.location.href)}`
 
     location.replace(authUrl)
-  }
-  if (window.location.hash.includes(GUEST_MARK)) {
-    history.replaceState(
-      window.history.state,
-      null,
-      urlHash({ remove: GUEST_MARK })
-    )
   }
 
   const app = createApp(App, {
